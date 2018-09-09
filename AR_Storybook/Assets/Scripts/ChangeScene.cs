dd29b66888413e9f890bc1aade7f5663a26ce992
Jolyn Wong <shiroyami08@gmail.com>
2018-09-10 07:54:01 +0800

    Created Marker and Markerless scenes

---
+++ AR_Storybook/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script handles Scene Changing ( implementation of loading screen if applicable )
+/// </summary>
+public class ChangeScene : MonoBehaviour
+{
+
+
+    /// <summary>
+    /// Changing to SceneMarker
+    /// </summary>
+    public void ChangeToSceneMarker()
+    {
+        SceneManager.LoadScene("SceneMarker");
+    }
+
+    /// <summary>
+    /// Changing to SceneMarkerless
+    /// </summary>
+    public void ChangeToSceneMarkerless()
+    {
+        SceneManager.LoadScene("SceneMarkerless");
+    }
+}
---
+++ AR_Storybook/Assets/Scripts/MarkerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script to handle SceneMarker ( will change to fit all marker scenes )
+/// </summary>
+public class MarkerController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
