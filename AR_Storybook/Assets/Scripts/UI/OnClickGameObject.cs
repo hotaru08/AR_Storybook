d9a52cdc865751e53c35b9de2dce99f8d63bc8a2
Wafieqa AZ <maguu.samurai@gmail.com>
2018-09-10 15:07:13 +0800

    Diegetic UI, Typewriter effect, blip sound effect

---
+++ AR_Storybook/Assets/Scripts/UI/OnClickGameObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickGameObject : MonoBehaviour {
+
+    [Tooltip("Set this GO to be active or inactive.")]
+    public GameObject GameobjectToBeSet;
+
+    [Tooltip("Set active or inactive.")]
+    public bool b_Active;
+
+    [Tooltip("Name of GO that is to be clicked")]
+    public string ClickableGameobjectName;
+
+    Ray ray;
+    RaycastHit hit;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameobjectToBeSet.activeSelf == true)
+            b_Active = true;
+        else
+            b_Active = false;
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name == ClickableGameobjectName)
+            {
+                if (Input.GetMouseButtonDown(0) && !b_Active) //if click and if not active
+                {
+                    print(hit.collider.name); //print in debug name of GO
+                    GameobjectToBeSet.SetActive(true); //Set the GO to be active
+                }
+            }
+            
+        }
+    }
+}
---
+++ AR_Storybook/Assets/Scripts/UI/TypewriterDialogue.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(TextMeshProUGUI))]
+
+public class TypewriterDialogue : MonoBehaviour {
+
+    public string dialogue = "Dialogue here";
+    private TextMeshProUGUI textComp;
+
+    [Tooltip("Delay at the begining")]
+    public float startDelay = 0f;
+
+    [Tooltip("Delay with the typing")]
+    public float typingDelay = 0.01f;
+
+    public AudioClip dialogueVoice;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("TypeIn");
+	}
+
+    void Awake()
+    {
+        textComp = GetComponent<TextMeshProUGUI>();
+    }
+
+    public IEnumerator TypeIn()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        for (int i = 0; i <= dialogue.Length; i++)
+        {
+            textComp.text = dialogue.Substring(0, i); //start index, index length
+            GetComponent<AudioSource>().PlayOneShot(dialogueVoice);
+            yield return new WaitForSeconds(typingDelay);
+        }
+    }
+
+    public IEnumerator TypeOff()
+    {
+        for (int i = dialogue.Length; i >= 0; i--) 
+        {
+            textComp.text = dialogue.Substring(0, i);
+            yield return new WaitForSeconds(typingDelay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
