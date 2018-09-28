a7579cd4b337a7ccbde1b494fab6d8b68af403b1
Adrian Tan <atxkirl@gmail.com>
2018-09-28 09:58:25 +0800

    Changelog: - Added node-based DialogueSystem (supports multiple branching storylines, abit of a pain in the arse to create the tree by hand, will be working on a node editor soon) - Added node-based custom editor (doesnt do anything now except place, remove and link nodes)

---
+++ AR_Storybook/Assets/ATXK/Node_Editor/Scripts/NE_ConnectionPoint.cs
@@ -0,0 +1,53 @@
+﻿namespace ATXK.NodeEditor
+{
+	using System;
+	using UnityEngine;
+
+	public enum ConnectionPointType { In, Out }
+
+	public class NE_ConnectionPoint
+	{
+		public Rect rect;
+
+		public ConnectionPointType type;
+
+		public NE_Node node;
+
+		public GUIStyle style;
+
+		public Action<NE_ConnectionPoint> OnClickConnectionPoint;
+
+		public NE_ConnectionPoint(NE_Node node, ConnectionPointType type, GUIStyle style, Action<NE_ConnectionPoint> OnClickConnectionPoint)
+		{
+			this.node = node;
+			this.type = type;
+			this.style = style;
+			this.OnClickConnectionPoint = OnClickConnectionPoint;
+			rect = new Rect(0, 0, 10f, 20f);
+		}
+
+		public void Draw()
+		{
+			rect.y = node.rect.y + (node.rect.height * 0.5f) - rect.height * 0.5f;
+
+			switch (type)
+			{
+				case ConnectionPointType.In:
+					rect.x = node.rect.x - rect.width + 8f;
+					break;
+
+				case ConnectionPointType.Out:
+					rect.x = node.rect.x + node.rect.width - 8f;
+					break;
+			}
+
+			if (GUI.Button(rect, "", style))
+			{
+				if (OnClickConnectionPoint != null)
+				{
+					OnClickConnectionPoint(this);
+				}
+			}
+		}
+	}
+}
