734a57cb4d09eba58817003fa317d5aa611b9a09
ATXK-WSPC <atxkirl@gmail.com>
2018-10-03 15:44:09 +0800

    Revert "Changelog:"
    This reverts commit d9d0e38f5267f330f2d0ef6a3120f03329bf2587.

--- AR_Storybook/Assets/ATXK/AI/Scripts/AI_Action.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ATXK.AI
-{
-	using UnityEngine;
-
-	public abstract class AI_Action : ScriptableObject
-	{
-		public abstract void Act(AI_Controller controller);
-	}
-}
--- AR_Storybook/Assets/ATXK/AI/Scripts/AI_Decision.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ATXK.AI
-{
-	using UnityEngine;
-
-	public abstract class AI_Decision : ScriptableObject
-	{
-		public abstract bool Decide(AI_Controller controller);
-	}
-}
