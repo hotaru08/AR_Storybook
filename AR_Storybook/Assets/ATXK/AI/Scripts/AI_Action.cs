d9d0e38f5267f330f2d0ef6a3120f03329bf2587
ATXK-WSPC <atxkirl@gmail.com>
2018-10-03 15:43:58 +0800

    Changelog: - Stubbed in scripts for scriptable object AI

---
+++ AR_Storybook/Assets/ATXK/AI/Scripts/AI_Action.cs
@@ -0,0 +1,9 @@
+﻿namespace ATXK.AI
+{
+	using UnityEngine;
+
+	public abstract class AI_Action : ScriptableObject
+	{
+		public abstract void Act(AI_Controller controller);
+	}
+}
---
+++ AR_Storybook/Assets/ATXK/AI/Scripts/AI_State.cs
@@ -0,0 +1,36 @@
+﻿namespace ATXK.AI
+{
+	using UnityEngine;
+	using System.Collections.Generic;
+
+	public class AI_State : ScriptableObject
+	{
+		List<AI_Action> actions;
+		List<AI_Transition> transitions;
+
+		public void UpdateState(AI_Controller controller)
+		{
+			DoActions(controller);
+			CheckTransitions(controller);
+		}
+
+		void DoActions(AI_Controller controller)
+		{
+			for(int i = actions.Count - 1; i >= 0; i--)
+			{
+
+			}
+		}
+
+		void CheckTransitions(AI_Controller controller)
+		{
+			for (int i = transitions.Count - 1; i >= 0; i--)
+			{
+				if(transitions[i].decision.Decide(controller))
+				{
+
+				}
+			}
+		}
+	}
+}
