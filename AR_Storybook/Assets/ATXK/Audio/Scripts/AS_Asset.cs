a358cbbeb6a2d0de256a79aa5762c225d5300a22
Adrian Tan <atxkirl@gmail.com>
2018-10-11 15:26:43 +0800

    Changelog: - Added a dinky Audio system thing...        - AS_Assets are asset files that hold a list of AudioClips that will be randomly selected when using the AS_Asset Play() or PlayOneShot() functions        - AS_AssetHolder is a component that references an AS_Asset file. The asset holder will handle calling the asset file's functions.        - Add the AS_AssetHolder component to any GameObject that needs to play Audio. Then attach a audio AS_Asset file to the holder component.
           - Example: Player needs to play audio when taking damage, so add the AS_AssetHolder component to the player and a "PlayerDamageSounds" asset to the holder.
           - Then add an ES_EventListener to play audio, when it receives a "DamagePlayer" event.

---
+++ AR_Storybook/Assets/ATXK/Audio/Scripts/AS_Asset.cs
@@ -0,0 +1,62 @@
+﻿namespace ATXK.Audio
+{
+	using UnityEngine;
+
+	[CreateAssetMenu(menuName = "Audio/Audio Asset")]
+	public class AS_Asset : ScriptableObject
+	{
+		public AudioClip[] AudioClips;
+		public float minVolume = 1f;
+		public float maxVolume = 1f;
+		public float minPitch = 1f;
+		public float maxPitch = 1f;
+		public bool loop = false;
+
+		AudioSource currentSource;
+
+		public void SetSource(AudioSource source)
+		{
+			if (currentSource == source)
+				return;
+
+			currentSource = source;
+			currentSource.clip = null;
+			currentSource.volume = Random.Range(minVolume, maxVolume);
+			currentSource.pitch = Random.Range(minPitch, maxPitch);
+			currentSource.loop = loop;
+		}
+
+		public void Play(AudioSource source)
+		{
+			SetSource(source);
+
+			if(!currentSource.isPlaying)
+			{
+				currentSource.clip = AudioClips[Random.Range(0, AudioClips.Length)];
+				currentSource.Play();
+			}
+		}
+
+		public void PlayOneShot(AudioSource source)
+		{
+			SetSource(source);
+
+			currentSource.PlayOneShot(AudioClips[Random.Range(0, AudioClips.Length)]);
+		}
+
+		public void Stop()
+		{
+			currentSource.Stop();
+		}
+
+		public void Pause()
+		{
+			currentSource.Pause();
+		}
+
+		public void Resume()
+		{
+			currentSource.UnPause();
+		}
+	}
+}
