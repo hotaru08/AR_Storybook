ef062e3ca2d9cb0ac7149e65d900d12473642564
Adrian Tan <atxkirl@gmail.com>
2018-10-17 17:02:51 +0800

    Changelog: - Particle thingy WIP

---
+++ AR_Storybook/Assets/ATXK/Particle_System/Scripts/PS_Controller.cs
@@ -0,0 +1,32 @@
+﻿namespace ATXK.Particles
+{
+	using UnityEngine;
+	using EventSystem;
+
+	[RequireComponent(typeof(ParticleSystem))]
+	[RequireComponent(typeof(ES_EventListener))]
+	public class PS_Controller : MonoBehaviour
+	{
+		ParticleSystem particleSystem;
+		ParticleSystem.MainModule mainModule;
+
+		private void Awake()
+		{
+			particleSystem = GetComponent<ParticleSystem>();
+			mainModule = particleSystem.main;
+		}
+
+		public void Play(bool looped)
+		{
+			mainModule.loop = looped;
+			particleSystem.Play();
+		}
+
+		public void PlayOneShot()
+		{
+			mainModule.loop = false;
+			particleSystem.Stop();
+			particleSystem.Play();
+		}
+	}
+}
