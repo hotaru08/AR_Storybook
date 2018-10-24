247d5b99116f60ab7eef764413707f0f8ca5c2d0
Jolyn Wong <shiroyami08@gmail.com>
2018-10-24 20:30:21 +0800

    Flow got disrupted, so redid the flow : - Main Menu -> MainScene ( okay ) - MainScene -> Main Menu ( okay )
    Added Button Mode Toggle to Main Menu Options

--- AR_Storybook/Assets/Scripts/UI/scrollbg.cs
+++ AR_Storybook/Assets/Scripts/UI/scrollbg.cs
@@ -14,7 +14,6 @@ public class scrollbg : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        //originalPos = transform.position;
         startPos = transform.position;
         m_camera = Camera.main;
 	}
@@ -22,20 +21,11 @@ public class scrollbg : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        //float newPos = Mathf.Repeat(Time.deltaTime * ScrollingSpeed, 50);
-        //transform.position = startPos + Vector2.right * newPos;
-
         transform.position += Vector3.right * Time.deltaTime * ScrollingSpeed;
         if (transform.position.x + transform.localScale.x * 0.5f 
             > m_sky.transform.position.x + m_sky.transform.localScale.x * 0.5f)
         {
             transform.position = startPos;
-            //Debug.Log("Out of sky cube");
         }
-        //Debug.Log("Pos: " + (transform.position.x + transform.localScale.x).ToString());
-        //Debug.Log("Scale: " + transform.localScale.x);
-        //Debug.Log("Sky Pos: " + (m_sky.transform.position.x + m_sky.transform.localScale.x * 0.5f).ToString());
-
-        //Debug.Log("Sky screen pos: " + m_camera.WorldToScreenPoint(m_sky.transform.position + m_sky.transform.localScale));
     }
 }
