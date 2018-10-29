635c73fe96ceb31ac503d4cb4566b465b8f2e606
Adrian Tan <atxkirl@gmail.com>
2018-10-29 12:09:56 +0800

    Changelog: - Stubbing in highscore/photobooth thing - Removed old debugs and slightly edited AI_Action_SpawnItem to break instead of return

---
+++ AR_Storybook/Assets/TestSpace/Photobooth/Scripts/Photobooth_Generic_Switcher.cs
@@ -0,0 +1,33 @@
+﻿namespace TestSpace
+{
+	using UnityEngine;
+
+	public abstract class Photobooth_Generic_Switcher<T> : MonoBehaviour
+	{
+		[SerializeField] protected int currIndex;
+		[SerializeField] protected T[] listOfSwitchableObject;
+
+		public int CurrentIndex { get { return currIndex; } }
+		public T CurrentObject { get { return listOfSwitchableObject[currIndex]; } }
+
+		public virtual void NextObject()
+		{
+			currIndex++;
+			if (currIndex > listOfSwitchableObject.Length - 1)
+				currIndex = 0;
+		}
+
+		public virtual void PrevObject()
+		{
+			currIndex--;
+			if (currIndex < 0)
+				currIndex = listOfSwitchableObject.Length - 1;
+		}
+
+		public virtual void Switch(int index)
+		{
+			if (index > 0 && index < listOfSwitchableObject.Length - 1)
+				currIndex = index;
+		}
+	}
+}
---
+++ AR_Storybook/Assets/TestSpace/Photobooth/Scripts/Photobooth_Photographer.cs
@@ -0,0 +1,36 @@
+﻿namespace TestSpace
+{
+	using UnityEngine;
+	using ATXK.EventSystem;
+	using System.Collections;
+
+	public class Photobooth_Photographer : MonoBehaviour
+	{
+		[SerializeField] ES_Event_Base takePhotoStartEvent;
+		[SerializeField] ES_Event_Base takePhotoEndEvent;
+
+		public void TakeScreenshot()
+		{
+			StartCoroutine(Screenshot());
+		}
+
+		private IEnumerator Screenshot()
+		{
+			takePhotoStartEvent.Invoke();
+
+			yield return new WaitForSeconds(0.2f);
+
+			string fileName = "";
+			fileName = System.DateTime.Now.ToString("d_M_yyyy_HHmmss");
+			Debug.Log("Filename:" + fileName);
+
+#if !UNITY_EDITOR
+			ScreenCapture.CaptureScreenshot(fileName + ".png");
+#endif
+
+			yield return new WaitForSeconds(0.2f);
+
+			takePhotoEndEvent.Invoke();
+		}
+	}
+}
