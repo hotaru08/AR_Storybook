6a6c74d7142b4b10ea5b7ddb049ba1e193c82be0
Adrian Tan <atxkirl@gmail.com>
2018-11-02 12:43:57 +0800

    Changelog: - Added all 3 Arcade scenes        - Need to replace/add appropriate models        - Need to replace the floor textures        - Scenes already have the correct enemies

--- AR_Storybook/Assets/Scripts/Game/ScrollingObjects.cs
+++ AR_Storybook/Assets/Scripts/Game/ScrollingObjects.cs
@@ -78,7 +78,7 @@ public class ScrollingObjects : MonoBehaviour
 		}
 
 		// If first in queue has reached the end, move back to start point
-		if (m_movingObjects.Peek().transform.position.z > m_TravelAlongObject.GetComponent<Renderer>().bounds.size.z)
+		if (m_movingObjects.Peek().transform.position.z >= m_TravelAlongObject.gameObject.GetComponent<Renderer>().bounds.size.z)
 		{
 			// Move _go back to starting position
 			m_movingObjects.Peek().transform.position = transform.position;
