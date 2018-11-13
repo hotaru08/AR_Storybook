b5df931d4de31929cfada652fda23cd57aff7295
ATXK-WSPC <atxkirl@gmail.com>
2018-11-13 15:24:51 +0800

    Changelog: - Modified Long Shit projectile to face correctly (just put the model under a parent object that holds the Item Holder script + Collider) - Added Item Despawner script to destroy projectiles (also added it to all game scenes, storymode + arcade)

---
+++ AR_Storybook/Assets/ATXK/Item_System/Scripts/Items/Item_Despawner.cs
@@ -0,0 +1,24 @@
+﻿namespace ATXK.ItemSystem
+{
+	using UnityEngine;
+	
+	[RequireComponent(typeof(BoxCollider))]
+	[RequireComponent(typeof(Rigidbody))]
+	public class Item_Despawner : MonoBehaviour
+	{
+		BoxCollider collider;
+
+		private void Awake()
+		{
+			collider = GetComponent<BoxCollider>();
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if(other.gameObject.GetComponent<Item_Holder>())
+			{
+				Destroy(other.gameObject);
+			}
+		}
+	}
+}
